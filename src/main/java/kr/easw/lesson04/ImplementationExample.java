fdce6c6195a72c085f7e51cc3c0b165cb728aaad
mk3058 <kmk3058@icloud.com>
2024-04-11 13:39:57 +0900

    ImplementationExample 솔루션 추가

--- src/main/java/kr/easw/lesson04/ImplementationExample.java
+++ src/main/java/kr/easw/lesson04/ImplementationExample.java
@@ -3,6 +3,7 @@ package kr.easw.lesson04;
 public class ImplementationExample {
 
     public static void main(String[] args) {
+        System.out.println(onTest(1000));
         if (onTest(1000).equals("TestValue - 34300")) {
             System.out.println("정답입니다.");
             return;
@@ -17,34 +18,37 @@ public class ImplementationExample {
 
 
     private static class TestClass {
+
         /**
-         * 해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-         * 다음 문자열을 반환해야 합니다.
-         *
+         * 해당 메서드는 다음과 같은 역할을 가져야 합니다 : 다음 문자열을 반환해야 합니다.
+         * <p>
          * "TestValue"
          */
         public String getString() {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return "TestValue";
         }
 
         /**
          * 해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-         *
+         * <p>
          * 두번째 값에 다음 연산을 거친 후, 첫번째 값에 " - "를 붙여야 합니다.
-         *
+         * <p>
          * 두번째 값을 2로 나눈 다음, 7을 곱하고, 3을 제곱한 후, 5로 나눠야 합니다.
-         *
+         * <p>
          * 그 후, 이 값을 반환해야 합니다.
-         *
+         * <p>
          * 예를 들어, first가 "TestValue"이고, data가 1000이였다면, 다음 값이 반환되어야 합니다 :
-         *
+         * <p>
          * "TestValue - 34300"
          *
          * @param first 맨 앞에 위치할 문자열
-         * @param data 계산이 진행되어야 할 값
+         * @param data  계산이 진행되어야 할 값
          */
         public String eval(String first, int data) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            first = first + " - ";
+            long res = (long) (Math.pow((data / 2 * 7), 3) / 5);
+
+            return first + res;
         }
     }
 }
