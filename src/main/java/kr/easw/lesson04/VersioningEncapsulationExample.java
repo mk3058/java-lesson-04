e1c1fd791b5c58181a37558f351ff1b02d3b93f1
mk3058 <kmk3058@icloud.com>
2024-04-11 13:06:34 +0900

    VersioningEncapsulationExample 솔루션 추가

--- src/main/java/kr/easw/lesson04/VersioningEncapsulationExample.java
+++ src/main/java/kr/easw/lesson04/VersioningEncapsulationExample.java
@@ -2,15 +2,14 @@ package kr.easw.lesson04;
 
 /**
  * 해당 클래스는 {@link TestInterface}를 상속받아 새 버전을 구현하는 코드를 가지고 있습니다.
- *
+ * <p>
  * 해당 문제에서는 {@link TestV1}, {@link TestV2}, {@link TestV3} 3개의 클래스를 수정해 정상적인 값을 도출하도록 해야 합니다.
- *
- * 이는 다음의 조건을 따라야 합니다 :
- * - TestV1 클래스는 입력된 문자열을 그대로 반환해야 합니다.
- * - TestV2 클래스는 입력된 문자열에서 느낌표(!)를 물음표(?)로 바꿔야 합니다.
- * - TestV3 클래스는 입력된 문자열에서 느낌표(!)를 물음표(?)로 바꾸고, 공백( )을 제거해야 합니다.
+ * <p>
+ * 이는 다음의 조건을 따라야 합니다 : - TestV1 클래스는 입력된 문자열을 그대로 반환해야 합니다. - TestV2 클래스는 입력된 문자열에서 느낌표(!)를 물음표(?)로
+ * 바꿔야 합니다. - TestV3 클래스는 입력된 문자열에서 느낌표(!)를 물음표(?)로 바꾸고, 공백( )을 제거해야 합니다.
  */
 public class VersioningEncapsulationExample {
+
     public static String VALUE = "Hello, World!";
 
     public static String RESULT_SECOND = "Hello, World?";
@@ -37,32 +36,36 @@ public class VersioningEncapsulationExample {
     }
 
     interface TestInterface {
+
         String doAction(String type);
     }
 
 
     static class TestV1 implements TestInterface {
+
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return type;
         }
     }
 
 
     // Split by space
     static class TestV2 implements TestInterface {
+
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return type.replaceAll("!", "?");
         }
     }
 
 
     // Split by space, and concat
     static class TestV3 implements TestInterface {
+
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return type.replaceAll("!", "?").replaceAll(" ", "");
         }
     }
 }
