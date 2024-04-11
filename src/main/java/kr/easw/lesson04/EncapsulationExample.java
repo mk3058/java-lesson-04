3fb6ba66d6d3ea3307392fecd469e2a439f4beda
mk3058 <kmk3058@icloud.com>
2024-04-11 13:18:17 +0900

    EncapsulationExample 솔루션 추가

--- src/main/java/kr/easw/lesson04/EncapsulationExample.java
+++ src/main/java/kr/easw/lesson04/EncapsulationExample.java
@@ -1,17 +1,18 @@
 package kr.easw.lesson04;
+
 /**
  * 해당 클래스는 Car을 상속받아 연비 값을 확인하는 코드를 갖고 있습니다.
- *
+ * <p>
  * 이 코드에서는 세터에 아무런 필터링이 존재하지 않아 이 값을 검증할 수 없습니다.
- *
+ * <p>
  * {@link Car} 클래스만을 수정해 {@link PerformanceManipulation} 클래스에서 수행하는 값 조작을 막아야 합니다.
- *
- * 이는 다음의 조건을 따라야 합니다 :
- * - 현재 연비보다 높은 값을 설정하려 할 경우, 종류 상관 없이 오류를 발생시켜야 합니다.
- *
+ * <p>
+ * 이는 다음의 조건을 따라야 합니다 : - 현재 연비보다 높은 값을 설정하려 할 경우, 종류 상관 없이 오류를 발생시켜야 합니다.
+ * <p>
  * 오류는 throw new 구문으로 발생시킬 수 있습니다.
  */
 public class EncapsulationExample {
+
     public static void main(String[] args) {
         try {
             Car car = getCar();
@@ -25,6 +26,7 @@ public class EncapsulationExample {
     }
 
     private static abstract class Car {
+
         private final String carName = "Car Prototype";
 
         private double realFuelEfficiency = 7.5;
@@ -38,11 +40,15 @@ public class EncapsulationExample {
         }
 
         public void setRealFuelEfficiency(double realFuelEfficiency) {
+            if (realFuelEfficiency > this.realFuelEfficiency) {
+                throw new IllegalArgumentException("현재 값 보다 더 높은 연비를 설정할 수 없습니다");
+            }
             this.realFuelEfficiency = realFuelEfficiency;
         }
     }
 
     private static class PerformanceManipulation extends Car {
+
         {
             setRealFuelEfficiency(15.0);
         }
